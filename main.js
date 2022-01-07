37a495cdff6bc1293e4db1af41d4fc78134953a2
RasmusBak <1062669@ucn.dk>
2022-01-07 22:30:53 +0100

    Done

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://www.happysocks.com/dk/'
+        }
+    }
+})
